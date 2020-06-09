6b96ff163ec5c6d9ea47b1aa81d8e33d1a11d91a
RonaldAndrei <ronaldandreyrnd@gmail.com>
2020-06-09 20:30:59 -0300

    Revert "Initial commit"
    This reverts commit 8b1a96e290ccecab215a798c8db5ab994865cd26.

---
+++ server/src/routes.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+
+import PointsController from './Controllers/PointsController';
+import ItemsController from './Controllers/ItemsController';
+
+const routes = express.Router();
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
+
+routes.get('/items', itemsController.index);
+
+routes.get('/points', pointsController.index);
+routes.get('/points/:id', pointsController.show);
+routes.post('/points', pointsController.create);
+
+export default routes;
